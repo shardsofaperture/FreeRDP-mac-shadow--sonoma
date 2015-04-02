shardsofaperture/FreeRDP-mac-shadow--sonoma

commit df1c523a26f6c2dcf0ac5233e7649a07e1f1f14c

cmdline: fix incorrect usage of realloc.
Signed-off-by: Zhang Zhaolong <zhangzl2013@126.com>

client/common/cmdline.c
@@ -544,10 +544,17 @@ static char** freerdp_command_line_parse_comma_separated_values(char* list, int*
 static char** freerdp_command_line_parse_comma_separated_values_offset(char* list, int* count)
 {
 	char** p;
+	char** t;
+	int s;
 
 	p = freerdp_command_line_parse_comma_separated_values(list, count);
+	if (!p)
+		return NULL;
 
-	p = (char**) realloc(p, sizeof(char*) * (*count + 1));
+	t = (char**) realloc(p, sizeof(char*) * (*count + 1));
+	if (!t)
+		return NULL;
+	p = t;
 	MoveMemory(&p[1], p, sizeof(char*) * *count);
 	(*count)++;
 

commit 8dba1c16840c90d21bc7bdb372130a97decb668c

Fix regression introduced in df1c523
This fixes problems with command line parsing.

client/common/cmdline.c
@@ -547,14 +547,13 @@ static char** freerdp_command_line_parse_comma_separated_values_offset(char* lis
 	char** t;
 
 	p = freerdp_command_line_parse_comma_separated_values(list, count);
-	if (!p)
-		return NULL;
 
 	t = (char**) realloc(p, sizeof(char*) * (*count + 1));
 	if (!t)
 		return NULL;
 	p = t;
-	MoveMemory(&p[1], p, sizeof(char*) * *count);
+	if (count > 0)
+        MoveMemory(&p[1], p, sizeof(char*) * *count);
 	(*count)++;
 
 	return p;