shardsofaperture/FreeRDP-mac-shadow--sonoma

commit 4f5645db1ddf9c976344ec0c1c4875ee821a2d42

Unlink file after binding to it.
When unlinking the file before binding, a new entry is created
in the file system after binding. This is not desireable, so
unlink it after binding to remove the temporary file after the process
closes.

libfreerdp/core/listener.c
@@ -185,9 +185,9 @@ static BOOL freerdp_listener_open_local(freerdp_listener* instance, const char* 
 
 	addr.sun_family = AF_UNIX;
 	strncpy(addr.sun_path, path, sizeof(addr.sun_path));
-	unlink(path);
 
 	status = _bind(sockfd, (struct sockaddr*) &addr, sizeof(addr));
+	unlink(path);
 
 	if (status != 0)
 	{

commit afb8cacf7f6f2d42755953edd07d9e546a6f30d9

Do not delete the listener socket right after creating it.
The listener server socket file needs to be deleted before we bind it
otherwise it's an "address already in use" error. But it was getting
deleted after the bind, causing the file to disappear, and preventing
anyone from connecting to the listener socket since the socket stops
existing.

This is caused by commit 4f5645db1ddf9c976344ec0c1c4875ee821a2d42

libfreerdp/core/listener.c
@@ -186,8 +186,8 @@ static BOOL freerdp_listener_open_local(freerdp_listener* instance, const char* 
 	addr.sun_family = AF_UNIX;
 	strncpy(addr.sun_path, path, sizeof(addr.sun_path));
 
-	status = _bind(sockfd, (struct sockaddr*) &addr, sizeof(addr));
 	unlink(path);
+	status = _bind(sockfd, (struct sockaddr*) &addr, sizeof(addr));
 
 	if (status != 0)
 	{