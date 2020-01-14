rons1/FreeRDP

server: proxy: rewrite modules API
* Add support for C++ plugins.
* Seperate between a "module" and a "plugin": a module is a shared
library that exposes a function called "proxy_module_entry_point". This
function gets a parameter to `proxyPluginsManager*`, which can be used to
register plugins.
* Refine the API of the modules infrastructure:
 * use proxyData* instead of rdpContext* when firing a hook or calling a filter.
 * use clearer names.
* Instead of having to specify a path for each module to be loaded in
the configuration, the proxy now loads modules from specificed
directory, using the CMAKE variable PROXY_PLUGINDIR.
* Add an option to specify required plugins: plugins that the proxy
wouldn't start without having them fully loaded.

server/proxy/modules/demo/demo.cpp
@@ -1,0 +1,64 @@
+/**
+ * FreeRDP: A Remote Desktop Protocol Implementation
+ * FreeRDP Proxy Server Demo C++ Module
+ *
+ * Copyright 2019 Kobi Mizrachi <kmizrachi18@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <iostream>
+
+#include "modules_api.h"
+
+#define TAG MODULE_TAG("demo")
+
+static constexpr char plugin_name[] = "demo";
+static constexpr char plugin_desc[] = "this is a test plugin";
+
+static proxyPluginsManager* g_plugins_manager = NULL;
+
+static BOOL demo_filter_keyboard_event(proxyData* pdata, void* param)
+{
+	auto event_data = static_cast<proxyKeyboardEventInfo*>(param);
+	if (event_data == NULL)
+		return FALSE;
+
+	if (event_data->rdp_scan_code == RDP_SCANCODE_KEY_B)
+	{
+		/* user typed 'B', that means bye :) */
+		std::cout << "C++ proxy plugin: aborting connection" << std::endl;
+		g_plugins_manager->AbortConnect(pdata);
+	}
+
+	return TRUE;
+}
+
+static proxyPlugin demo_plugin = {
+	plugin_name,                /* name */
+	plugin_desc,                /* description */
+	NULL,                       /* ClientPreConnect */
+	NULL,                       /* ClientLoginFailure */
+	NULL,                       /* ServerPostConnect */
+	NULL,                       /* ServerChannelsInit */
+	NULL,                       /* ServerChannelsFree */
+	demo_filter_keyboard_event, /* KeyboardEvent */
+	NULL                        /* MouseEvent */
+};
+
+BOOL proxy_module_entry_point(proxyPluginsManager* plugins_manager)
+{
+	g_plugins_manager = plugins_manager;
+
+	return plugins_manager->RegisterPlugin(&demo_plugin);
+}

shardsofaperture/FreeRDP-mac-shadow--sonoma

server/proxy/modules/demo/demo.cpp
@@ -1,0 +1,71 @@
+/**
+ * FreeRDP: A Remote Desktop Protocol Implementation
+ * FreeRDP Proxy Server Demo C++ Module
+ *
+ * Copyright 2019 Kobi Mizrachi <kmizrachi18@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <iostream>
+
+#include "modules_api.h"
+
+#define TAG MODULE_TAG("demo")
+
+static constexpr char plugin_name[] = "demo";
+static constexpr char plugin_desc[] = "this is a test plugin";
+
+static proxyPluginsManager* g_plugins_manager = NULL;
+
+static BOOL demo_filter_keyboard_event(proxyData* pdata, void* param)
+{
+	auto event_data = static_cast<proxyKeyboardEventInfo*>(param);
+	if (event_data == NULL)
+		return FALSE;
+
+	if (event_data->rdp_scan_code == RDP_SCANCODE_KEY_B)
+	{
+		/* user typed 'B', that means bye :) */
+		std::cout << "C++ demo plugin: aborting connection" << std::endl;
+		g_plugins_manager->AbortConnect(pdata);
+	}
+
+	return TRUE;
+}
+
+static BOOL demo_plugin_unload()
+{
+	std::cout << "C++ demo plugin: unloading..." << std::endl;
+	return TRUE;
+}
+
+static proxyPlugin demo_plugin = {
+	plugin_name,                /* name */
+	plugin_desc,                /* description */
+	NULL,                       /* ClientPreConnect */
+	NULL,                       /* ClientLoginFailure */
+	NULL,                       /* ServerPostConnect */
+	NULL,                       /* ServerChannelsInit */
+	NULL,                       /* ServerChannelsFree */
+	demo_filter_keyboard_event, /* KeyboardEvent */
+	NULL,                       /* MouseEvent */
+	demo_plugin_unload          /* PluginUnload */
+};
+
+BOOL proxy_module_entry_point(proxyPluginsManager* plugins_manager)
+{
+	g_plugins_manager = plugins_manager;
+
+	return plugins_manager->RegisterPlugin(&demo_plugin);
+}