FreeRDP/FreeRDP

[winpr,cast] split cast macros to header
* Simplify winpr/assert.h to have the least possible includes
* Move some definitions to winpr/platform.h
* Split and move cast macros to winpr/cast.h
* Add new cast macro for integer casts

winpr/include/winpr/cast.h
@@ -1,0 +1,121 @@
+/**
+ * WinPR: Windows Portable Runtime
+ * Cast macros
+ *
+ * Copyright 2024 Armin Novak <anovak@thincast.com>
+ * Copyright 2024 Thincast Technologies GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <stdint.h>
+
+#include <winpr/assert-light.h>
+
+/**
+ * @brief C++ safe cast macro
+ * @since version 3.10.1
+ */
+#ifdef __cplusplus
+#define WINPR_CXX_SAFE_CAST(t, val) static_cast<t>(val)
+#else
+#define WINPR_CXX_SAFE_CAST(t, val) (t)(val)
+#endif
+
+#if defined(__GNUC__) || defined(__clang__)
+/**
+ * @brief A macro to do dirty casts. Do not use without a good justification!
+ * @param ptr The pointer to cast
+ * @param dstType The data type to cast to
+ * @return The casted pointer
+ * @since version 3.9.0
+ */
+#define WINPR_REINTERPRET_CAST(ptr, srcType, dstType)            \
+	__extension__({                                              \
+		union                                                    \
+		{                                                        \
+			srcType src;                                         \
+			dstType dst;                                         \
+		} cnv;                                                   \
+		WINPR_STATIC_ASSERT(sizeof(srcType) == sizeof(dstType)); \
+		cnv.src = ptr;                                           \
+		cnv.dst;                                                 \
+	})
+
+/**
+ * @brief A macro to do dirty casts. Do not use without a good justification!
+ * @param ptr The pointer to cast
+ * @param dstType The data type to cast to
+ * @return The casted pointer
+ * @since version 3.9.0
+ */
+#define WINPR_CAST_CONST_PTR_AWAY(ptr, dstType) \
+	__extension__({                             \
+		union                                   \
+		{                                       \
+			__typeof(ptr) src;                  \
+			dstType dst;                        \
+		} cnv;                                  \
+		cnv.src = ptr;                          \
+		cnv.dst;                                \
+	})
+
+/**
+ * @brief A macro to do function pointer casts. Do not use without a good justification!
+ * @param ptr The pointer to cast
+ * @param dstType The data type to cast to
+ * @return The casted pointer
+ * @since version 3.9.0
+ */
+#define WINPR_FUNC_PTR_CAST(ptr, dstType) \
+	__extension__({                       \
+		union                             \
+		{                                 \
+			__typeof(ptr) src;            \
+			dstType dst;                  \
+		} cnv;                            \
+		cnv.src = ptr;                    \
+		cnv.dst;                          \
+	})
+
+#else
+#define WINPR_REINTERPRET_CAST(ptr, srcType, dstType) (dstType) ptr
+#define WINPR_CAST_CONST_PTR_AWAY(ptr, dstType) (dstType) ptr
+#define WINPR_FUNC_PTR_CAST(ptr, dstType) (dstType)(uintptr_t) ptr
+#endif
+
+#if defined(__GNUC__) || defined(__clang__)
+
+/**
+ * @brief A macro to do checked integer casts.
+ * will check if the value does change by casting to and from the target type and comparing the
+ * values. will also check if the sign of a value changes during conversion.
+ *
+ * @param type the type to cast to
+ * @param var the integer of unknown type to cast
+ * @return The casted integer
+ * @since version 3.10.1
+ */
+#define WINPR_SAFE_INT_CAST(type, var)                                                      \
+	__extension__({                                                                         \
+		WINPR_ASSERT((var) ==                                                               \
+		             WINPR_CXX_SAFE_CAST(__typeof(var), WINPR_CXX_SAFE_CAST(type, (var)))); \
+		WINPR_ASSERT((((var) > 0) && (WINPR_CXX_SAFE_CAST(type, (var)) > 0)) ||             \
+		             (((var) <= 0) && WINPR_CXX_SAFE_CAST(type, (var)) <= 0));              \
+		WINPR_CXX_SAFE_CAST(type, (var));                                                   \
+	})
+
+#else
+#define WINPR_SAFE_INT_CAST(type, var) WINPR_CXX_SAFE_CAST(type, var)
+#endif

winpr/include/winpr/crt.h
@@ -24,6 +24,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <winpr/cast.h>
 #include <winpr/platform.h>
 #include <winpr/winpr.h>
 
@@ -108,13 +109,7 @@ static INLINE UINT64 _byteswap_uint64(UINT64 _val)
 
 static INLINE UINT16 _byteswap_ushort(UINT16 _val)
 {
-#ifdef __cplusplus
-#define winpr_byteswap_cast(t, val) static_cast<t>(val)
-#else
-#define winpr_byteswap_cast(t, val) (t)(val)
-#endif
-	return winpr_byteswap_cast(UINT16, ((_val) >> 8U) | ((_val) << 8U));
-#undef winpr_byteswap_cast
+	return WINPR_CXX_SAFE_CAST(UINT16, ((_val) >> 8U) | ((_val) << 8U));
 }
 
 #endif /* (__GNUC__ > 4) || ... */

winpr/include/winpr/endian.h
@@ -26,12 +26,9 @@
 #include <winpr/wtypes.h>
 #include <winpr/platform.h>
 #include <winpr/assert.h>
+#include <winpr/cast.h>
 
-#ifdef __cplusplus
-#define WINPR_ENDIAN_CAST(t, val) static_cast<t>(val)
-#else
-#define WINPR_ENDIAN_CAST(t, val) (t)(val)
-#endif
+#define WINPR_ENDIAN_CAST(t, val) WINPR_CXX_SAFE_CAST(t, val)
 
 #ifdef __cplusplus
 extern "C"

winpr/include/winpr/error.h
@@ -20,8 +20,7 @@
 #ifndef WINPR_ERROR_H
 #define WINPR_ERROR_H
 
-#include <winpr/platform.h>
-#include <winpr/winpr.h>
+#include <winpr/cast.h>
 #include <winpr/wtypes.h>
 
 #ifdef _WIN32
@@ -147,11 +146,8 @@
 WINPR_PRAGMA_DIAG_PUSH
 WINPR_PRAGMA_DIAG_IGNORED_RESERVED_ID_MACRO
 
-#ifdef __cplusplus
-#define ERROR_CAST(t, val) static_cast<t>(val)
-#else
-#define ERROR_CAST(t, val) (t)(val)
-#endif
+#define ERROR_CAST(t, val) WINPR_CXX_SAFE_CAST(t, val)
+
 static INLINE HRESULT HRESULT_FROM_WIN32(unsigned long x)
 {
 	HRESULT hx = ERROR_CAST(HRESULT, x);

winpr/include/winpr/nt.h
@@ -23,12 +23,9 @@
 #include <winpr/winpr.h>
 #include <winpr/wtypes.h>
 #include <winpr/windows.h>
+#include <winpr/cast.h>
 
-#ifdef __cplusplus
-#define STATUS_CAST(t, val) static_cast<t>(val)
-#else
-#define STATUS_CAST(t, val) (t)(val)
-#endif
+#define STATUS_CAST(t, val) WINPR_CXX_SAFE_CAST(t, val)
 
 #ifndef _WIN32
 

winpr/include/winpr/stream.h
@@ -28,6 +28,8 @@
 #include <winpr/endian.h>
 #include <winpr/synch.h>
 #include <winpr/assert.h>
+#include <winpr/cast.h>
+#include <winpr/wlog.h>
 
 #ifdef __cplusplus
 extern "C"
@@ -56,11 +58,7 @@ extern "C"
 	WINPR_API BOOL Stream_EnsureCapacity(wStream* s, size_t size);
 	WINPR_API BOOL Stream_EnsureRemainingCapacity(wStream* s, size_t size);
 
-#ifdef __cplusplus
-#define WINPR_STREAM_CAST(t, val) static_cast<t>(val)
-#else
-#define WINPR_STREAM_CAST(t, val) (t)(val)
-#endif
+#define WINPR_STREAM_CAST(t, val) WINPR_CXX_SAFE_CAST(t, val)
 
 #define Stream_CheckAndLogRequiredCapacityOfSize(tag, s, nmemb, size)                         \
 	Stream_CheckAndLogRequiredCapacityEx(tag, WLOG_WARN, s, nmemb, size, "%s(%s:%" PRIuz ")", \
@@ -1252,7 +1250,7 @@ extern "C"
  *
  *  @since version 3.9.0
  */
-#define Stream_GetBufferAs(_s, _b) _b = Stream_BufferAs(_s, typeof(_b))
+#define Stream_GetBufferAs(_s, _b) _b = Stream_BufferAs(_s, __typeof(_b))
 
 #define Stream_PointerAs(s, type) WINPR_STREAM_CAST(type*, Stream_Pointer(s))
 
@@ -1274,7 +1272,7 @@ extern "C"
  *
  *  @since version 3.9.0
  */
-#define Stream_GetPointerAs(_s, _p) _p = Stream_PointerAs(_s, typeof(_p))
+#define Stream_GetPointerAs(_s, _p) _p = Stream_PointerAs(_s, __typeof(_p))
 
 #if defined(WITH_WINPR_DEPRECATED)
 	WINPR_API WINPR_DEPRECATED_VAR("Use Stream_SetPosition instead",

shardsofaperture/FreeRDP-mac-shadow--sonoma

winpr/include/winpr/cast.h
@@ -1,0 +1,122 @@
+/**
+ * WinPR: Windows Portable Runtime
+ * Cast macros
+ *
+ * Copyright 2024 Armin Novak <anovak@thincast.com>
+ * Copyright 2024 Thincast Technologies GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <stdint.h>
+
+#include <winpr/assert-api.h>
+
+/**
+ * @brief C++ safe cast macro
+ * @since version 3.10.1
+ */
+#ifdef __cplusplus
+#define WINPR_CXX_COMPAT_CAST(t, val) static_cast<t>(val)
+#else
+#define WINPR_CXX_COMPAT_CAST(t, val) (t)(val)
+#endif
+
+#if defined(__GNUC__) || defined(__clang__)
+/**
+ * @brief A macro to do dirty casts. Do not use without a good justification!
+ * @param ptr The pointer to cast
+ * @param dstType The data type to cast to
+ * @return The casted pointer
+ * @since version 3.9.0
+ */
+#define WINPR_REINTERPRET_CAST(ptr, srcType, dstType)            \
+	__extension__({                                              \
+		union                                                    \
+		{                                                        \
+			srcType src;                                         \
+			dstType dst;                                         \
+		} cnv;                                                   \
+		WINPR_STATIC_ASSERT(sizeof(srcType) == sizeof(dstType)); \
+		cnv.src = ptr;                                           \
+		cnv.dst;                                                 \
+	})
+
+/**
+ * @brief A macro to do dirty casts. Do not use without a good justification!
+ * @param ptr The pointer to cast
+ * @param dstType The data type to cast to
+ * @return The casted pointer
+ * @since version 3.9.0
+ */
+#define WINPR_CAST_CONST_PTR_AWAY(ptr, dstType) \
+	__extension__({                             \
+		union                                   \
+		{                                       \
+			__typeof(ptr) src;                  \
+			dstType dst;                        \
+		} cnv;                                  \
+		cnv.src = ptr;                          \
+		cnv.dst;                                \
+	})
+
+/**
+ * @brief A macro to do function pointer casts. Do not use without a good justification!
+ * @param ptr The pointer to cast
+ * @param dstType The data type to cast to
+ * @return The casted pointer
+ * @since version 3.9.0
+ */
+#define WINPR_FUNC_PTR_CAST(ptr, dstType)                              \
+	__extension__({                                                    \
+		union                                                          \
+		{                                                              \
+			__typeof(ptr) src;                                         \
+			dstType dst;                                               \
+		} cnv;                                                         \
+		WINPR_STATIC_ASSERT(sizeof(dstType) == sizeof(__typeof(ptr))); \
+		cnv.src = ptr;                                                 \
+		cnv.dst;                                                       \
+	})
+
+#else
+#define WINPR_REINTERPRET_CAST(ptr, srcType, dstType) (dstType) ptr
+#define WINPR_CAST_CONST_PTR_AWAY(ptr, dstType) (dstType) ptr
+#define WINPR_FUNC_PTR_CAST(ptr, dstType) (dstType)(uintptr_t) ptr
+#endif
+
+#if defined(__GNUC__) || defined(__clang__)
+
+/**
+ * @brief A macro to do checked integer casts.
+ * will check if the value does change by casting to and from the target type and comparing the
+ * values. will also check if the sign of a value changes during conversion.
+ *
+ * @param type the type to cast to
+ * @param var the integer of unknown type to cast
+ * @return The casted integer
+ * @since version 3.10.1
+ */
+#define WINPR_ASSERTING_INT_CAST(type, var)                                                     \
+	__extension__({                                                                             \
+		WINPR_ASSERT((var) ==                                                                   \
+		             WINPR_CXX_COMPAT_CAST(__typeof(var), WINPR_CXX_COMPAT_CAST(type, (var)))); \
+		WINPR_ASSERT((((var) > 0) && (WINPR_CXX_COMPAT_CAST(type, (var)) > 0)) ||               \
+		             (((var) <= 0) && WINPR_CXX_COMPAT_CAST(type, (var)) <= 0));                \
+		WINPR_CXX_COMPAT_CAST(type, (var));                                                     \
+	})
+
+#else
+#define WINPR_ASSERTING_INT_CAST(type, var) WINPR_CXX_COMPAT_CAST(type, var)
+#endif

winpr/include/winpr/crt.h
@@ -24,6 +24,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <winpr/cast.h>
 #include <winpr/platform.h>
 #include <winpr/winpr.h>
 
@@ -108,13 +109,7 @@ static INLINE UINT64 _byteswap_uint64(UINT64 _val)
 
 static INLINE UINT16 _byteswap_ushort(UINT16 _val)
 {
-#ifdef __cplusplus
-#define winpr_byteswap_cast(t, val) static_cast<t>(val)
-#else
-#define winpr_byteswap_cast(t, val) (t)(val)
-#endif
-	return winpr_byteswap_cast(UINT16, ((_val) >> 8U) | ((_val) << 8U));
-#undef winpr_byteswap_cast
+	return WINPR_CXX_COMPAT_CAST(UINT16, ((_val) >> 8U) | ((_val) << 8U));
 }
 
 #endif /* (__GNUC__ > 4) || ... */

winpr/include/winpr/endian.h
@@ -26,12 +26,9 @@
 #include <winpr/wtypes.h>
 #include <winpr/platform.h>
 #include <winpr/assert.h>
+#include <winpr/cast.h>
 
-#ifdef __cplusplus
-#define WINPR_ENDIAN_CAST(t, val) static_cast<t>(val)
-#else
-#define WINPR_ENDIAN_CAST(t, val) (t)(val)
-#endif
+#define WINPR_ENDIAN_CAST(t, val) WINPR_CXX_COMPAT_CAST(t, val)
 
 #ifdef __cplusplus
 extern "C"

winpr/include/winpr/error.h
@@ -20,8 +20,7 @@
 #ifndef WINPR_ERROR_H
 #define WINPR_ERROR_H
 
-#include <winpr/platform.h>
-#include <winpr/winpr.h>
+#include <winpr/cast.h>
 #include <winpr/wtypes.h>
 
 #ifdef _WIN32
@@ -147,11 +146,8 @@
 WINPR_PRAGMA_DIAG_PUSH
 WINPR_PRAGMA_DIAG_IGNORED_RESERVED_ID_MACRO
 
-#ifdef __cplusplus
-#define ERROR_CAST(t, val) static_cast<t>(val)
-#else
-#define ERROR_CAST(t, val) (t)(val)
-#endif
+#define ERROR_CAST(t, val) WINPR_CXX_COMPAT_CAST(t, val)
+
 static INLINE HRESULT HRESULT_FROM_WIN32(unsigned long x)
 {
 	HRESULT hx = ERROR_CAST(HRESULT, x);

winpr/include/winpr/nt.h
@@ -23,12 +23,9 @@
 #include <winpr/winpr.h>
 #include <winpr/wtypes.h>
 #include <winpr/windows.h>
+#include <winpr/cast.h>
 
-#ifdef __cplusplus
-#define STATUS_CAST(t, val) static_cast<t>(val)
-#else
-#define STATUS_CAST(t, val) (t)(val)
-#endif
+#define STATUS_CAST(t, val) WINPR_CXX_COMPAT_CAST(t, val)
 
 #ifndef _WIN32
 

winpr/include/winpr/stream.h
@@ -28,6 +28,8 @@
 #include <winpr/endian.h>
 #include <winpr/synch.h>
 #include <winpr/assert.h>
+#include <winpr/cast.h>
+#include <winpr/wlog.h>
 
 #ifdef __cplusplus
 extern "C"
@@ -56,11 +58,7 @@ extern "C"
 	WINPR_API BOOL Stream_EnsureCapacity(wStream* s, size_t size);
 	WINPR_API BOOL Stream_EnsureRemainingCapacity(wStream* s, size_t size);
 
-#ifdef __cplusplus
-#define WINPR_STREAM_CAST(t, val) static_cast<t>(val)
-#else
-#define WINPR_STREAM_CAST(t, val) (t)(val)
-#endif
+#define WINPR_STREAM_CAST(t, val) WINPR_CXX_COMPAT_CAST(t, val)
 
 #define Stream_CheckAndLogRequiredCapacityOfSize(tag, s, nmemb, size)                         \
 	Stream_CheckAndLogRequiredCapacityEx(tag, WLOG_WARN, s, nmemb, size, "%s(%s:%" PRIuz ")", \
@@ -1252,7 +1250,7 @@ extern "C"
  *
  *  @since version 3.9.0
  */
-#define Stream_GetBufferAs(_s, _b) _b = Stream_BufferAs(_s, typeof(_b))
+#define Stream_GetBufferAs(_s, _b) _b = Stream_BufferAs(_s, __typeof(_b))
 
 #define Stream_PointerAs(s, type) WINPR_STREAM_CAST(type*, Stream_Pointer(s))
 
@@ -1274,7 +1272,7 @@ extern "C"
  *
  *  @since version 3.9.0
  */
-#define Stream_GetPointerAs(_s, _p) _p = Stream_PointerAs(_s, typeof(_p))
+#define Stream_GetPointerAs(_s, _p) _p = Stream_PointerAs(_s, __typeof(_p))
 
 #if defined(WITH_WINPR_DEPRECATED)
 	WINPR_API WINPR_DEPRECATED_VAR("Use Stream_SetPosition instead",