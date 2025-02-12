akallabeth/FreeRDP

[simd] move CPU feature detection
Use static inline functions in header to do CPU feature detection.
The c files are already compiled/linked with SIMD support and might have
used instructions from that featureset already.

libfreerdp/primitives/prim_YUV.h
@@ -22,10 +22,34 @@
 #define FREERDP_LIB_PRIM_YUV_H
 
 #include <winpr/wtypes.h>
+#include <winpr/sysinfo.h>
+
 #include <freerdp/config.h>
 #include <freerdp/primitives.h>
 
-void primitives_init_YUV_sse41(primitives_t* WINPR_RESTRICT prims);
-void primitives_init_YUV_neon(primitives_t* WINPR_RESTRICT prims);
+#include "prim_internal.h"
+
+static inline void primitives_init_YUV_sse41(primitives_t* WINPR_RESTRICT prims)
+{
+	primitives_init_YUV(prims);
+
+	if (!IsProcessorFeaturePresentEx(PF_EX_SSE41) ||
+	    !IsProcessorFeaturePresent(PF_SSE4_1_INSTRUCTIONS_AVAILABLE))
+		return;
+
+	extern void primitives_init_YUV_sse41_int(primitives_t * WINPR_RESTRICT prims);
+	primitives_init_YUV_sse41_int(prims);
+}
+
+static inline void primitives_init_YUV_neon(primitives_t* WINPR_RESTRICT prims)
+{
+	primitives_init_YUV(prims);
+
+	if (!IsProcessorFeaturePresentEx(PF_EX_SSE41))
+		return;
+
+	extern void primitives_init_YUV_neon_int(primitives_t * WINPR_RESTRICT prims);
+	primitives_init_YUV_neon_int(prims);
+}
 
 #endif

libfreerdp/primitives/prim_alphaComp.h
@@ -22,9 +22,23 @@
 #define FREERDP_LIB_PRIM_ALPHA_COMP_H
 
 #include <winpr/wtypes.h>
+#include <winpr/sysinfo.h>
+
 #include <freerdp/config.h>
 #include <freerdp/primitives.h>
 
-void primitives_init_alphaComp_sse3(primitives_t* WINPR_RESTRICT prims);
+#include "prim_internal.h"
+
+static inline void primitives_init_alphaComp_sse3(primitives_t* WINPR_RESTRICT prims)
+{
+	primitives_init_alphaComp(prims);
+
+	if (!IsProcessorFeaturePresent(PF_SSE2_INSTRUCTIONS_AVAILABLE) ||
+	    !IsProcessorFeaturePresent(PF_SSE3_INSTRUCTIONS_AVAILABLE)) /* for LDDQU */
+		return;
+
+	extern void primitives_init_alphaComp_sse3_int(primitives_t * WINPR_RESTRICT prims);
+	primitives_init_alphaComp_sse3_int(prims);
+}
 
 #endif

shardsofaperture/FreeRDP-mac-shadow--sonoma

libfreerdp/primitives/prim_YUV.h
@@ -22,10 +22,34 @@
 #define FREERDP_LIB_PRIM_YUV_H
 
 #include <winpr/wtypes.h>
+#include <winpr/sysinfo.h>
+
 #include <freerdp/config.h>
 #include <freerdp/primitives.h>
 
-void primitives_init_YUV_sse41(primitives_t* WINPR_RESTRICT prims);
-void primitives_init_YUV_neon(primitives_t* WINPR_RESTRICT prims);
+#include "prim_internal.h"
+
+FREERDP_LOCAL void primitives_init_YUV_sse41_int(primitives_t* WINPR_RESTRICT prims);
+static inline void primitives_init_YUV_sse41(primitives_t* WINPR_RESTRICT prims)
+{
+	primitives_init_YUV(prims);
+
+	if (!IsProcessorFeaturePresentEx(PF_EX_SSE41) ||
+	    !IsProcessorFeaturePresent(PF_SSE4_1_INSTRUCTIONS_AVAILABLE))
+		return;
+
+	primitives_init_YUV_sse41_int(prims);
+}
+
+FREERDP_LOCAL void primitives_init_YUV_neon_int(primitives_t* WINPR_RESTRICT prims);
+static inline void primitives_init_YUV_neon(primitives_t* WINPR_RESTRICT prims)
+{
+	primitives_init_YUV(prims);
+
+	if (!IsProcessorFeaturePresentEx(PF_EX_SSE41))
+		return;
+
+	primitives_init_YUV_neon_int(prims);
+}
 
 #endif

libfreerdp/primitives/prim_alphaComp.h
@@ -22,9 +22,23 @@
 #define FREERDP_LIB_PRIM_ALPHA_COMP_H
 
 #include <winpr/wtypes.h>
+#include <winpr/sysinfo.h>
+
 #include <freerdp/config.h>
 #include <freerdp/primitives.h>
 
-void primitives_init_alphaComp_sse3(primitives_t* WINPR_RESTRICT prims);
+#include "prim_internal.h"
+
+FREERDP_LOCAL void primitives_init_alphaComp_sse3_int(primitives_t* WINPR_RESTRICT prims);
+static inline void primitives_init_alphaComp_sse3(primitives_t* WINPR_RESTRICT prims)
+{
+	primitives_init_alphaComp(prims);
+
+	if (!IsProcessorFeaturePresent(PF_SSE2_INSTRUCTIONS_AVAILABLE) ||
+	    !IsProcessorFeaturePresent(PF_SSE3_INSTRUCTIONS_AVAILABLE)) /* for LDDQU */
+		return;
+
+	primitives_init_alphaComp_sse3_int(prims);
+}
 
 #endif