FreeRDP/FreeRDP

[client,x11] use rel mouse events only when no cursor set

client/X11/xf_graphics.c
@@ -380,6 +380,7 @@ static BOOL xf_Pointer_Set(rdpContext* context, rdpPointer* pointer)
 		WLog_WARN(TAG, "handle=%ld", handle);
 	}
 #endif
+	xfc->hasCursor = true;
 	return TRUE;
 }
 
@@ -412,6 +413,7 @@ static BOOL xf_Pointer_SetNull(rdpContext* context)
 
 	xf_unlock_x11(xfc);
 #endif
+	xfc->hasCursor = false;
 	return TRUE;
 }
 
@@ -429,6 +431,7 @@ static BOOL xf_Pointer_SetDefault(rdpContext* context)
 
 	xf_unlock_x11(xfc);
 #endif
+	xfc->hasCursor = true;
 	return TRUE;
 }
 

shardsofaperture/FreeRDP-mac-shadow--sonoma

client/X11/xf_graphics.c
@@ -380,6 +380,7 @@ static BOOL xf_Pointer_Set(rdpContext* context, rdpPointer* pointer)
 		WLog_WARN(TAG, "handle=%ld", handle);
 	}
 #endif
+	xfc->isCursorHidden = false;
 	return TRUE;
 }
 
@@ -412,6 +413,7 @@ static BOOL xf_Pointer_SetNull(rdpContext* context)
 
 	xf_unlock_x11(xfc);
 #endif
+	xfc->isCursorHidden = true;
 	return TRUE;
 }
 
@@ -429,6 +431,7 @@ static BOOL xf_Pointer_SetDefault(rdpContext* context)
 
 	xf_unlock_x11(xfc);
 #endif
+	xfc->isCursorHidden = false;
 	return TRUE;
 }
 