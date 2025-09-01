shardsofaperture/FreeRDP-mac-shadow--sonoma

commit 8d4a399ed318dc921f2c5103c13d84152da66784

core/redirection: Ensure stream has enough space for the certificate
Instead of checking whether enough space for the certificate is
available, simply use Stream_EnsureRemainingCapacity() to extend the
buffer size if needed. Otherwise, server redirection might fail despite
having a valid certificate.

See also: https://gitlab.gnome.org/GNOME/gnome-remote-desktop/-/issues/274

libfreerdp/core/redirection.c
@@ -339,7 +339,7 @@ static BOOL rdp_target_cert_write_element(wStream* s, UINT32 Type, UINT32 Encodi
 	Stream_Write_UINT32(s, Encoding);
 	Stream_Write_UINT32(s, (UINT32)length);
 
-	if (!Stream_CheckAndLogRequiredCapacity(TAG, s, length))
+	if (!Stream_EnsureRemainingCapacity(s, length))
 		return FALSE;
 
 	Stream_Write(s, data, length);

commit 2eb28796ee8064c753d07c8051304e2e7c471064

core/redirection: Ensure stream has enough space for all parameters
While commit 8d4a399ed318dc921f2c5103c13d84152da66784 fixed the case,
where the stream did not have enough space for the target certificate,
it did miss out a few other cases where the remaining length is just
checked, but not increased when needed. Fix this by now also covering
the remaining cases.

Fixes: 8d4a399ed318dc921f2c5103c13d84152da66784

libfreerdp/core/redirection.c
@@ -203,12 +203,12 @@ static BOOL rdp_redirection_write_data(wStream* s, size_t length, const void* da
 	WINPR_ASSERT(data || (length == 0));
 	WINPR_ASSERT(length <= UINT32_MAX);
 
-	if (!Stream_CheckAndLogRequiredCapacity(TAG, s, 4))
+	if (!Stream_EnsureRemainingCapacity(s, 4))
 		return FALSE;
 
 	Stream_Write_UINT32(s, (UINT32)length);
 
-	if (!Stream_CheckAndLogRequiredCapacity(TAG, s, length))
+	if (!Stream_EnsureRemainingCapacity(s, length))
 		return FALSE;
 
 	Stream_Write(s, data, length);
@@ -332,7 +332,7 @@ static BOOL rdp_target_cert_write_element(wStream* s, UINT32 Type, UINT32 Encodi
 	WINPR_ASSERT(data || (length == 0));
 	WINPR_ASSERT(length <= UINT32_MAX);
 
-	if (!Stream_CheckAndLogRequiredCapacity(TAG, s, 12))
+	if (!Stream_EnsureRemainingCapacity(s, 12))
 		return FALSE;
 
 	Stream_Write_UINT32(s, Type);