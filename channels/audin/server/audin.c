shardsofaperture/FreeRDP-mac-shadow--sonoma

commit 20ed264d5d6195311f3685344f9445324ceeb8ef

[channels,audin] fix nodiscard warnings

channels/audin/server/audin.c
@@ -391,16 +391,16 @@ static DWORD WINAPI audin_server_thread_func(LPVOID arg)
 		if (!Stream_EnsureRemainingCapacity(s, BytesReturned))
 			break;
 
-		WINPR_ASSERT(Stream_Capacity(s) <= UINT32_MAX);
-		if (WTSVirtualChannelRead(audin->audin_channel, 0, Stream_BufferAs(s, char),
-		                          (ULONG)Stream_Capacity(s), &BytesReturned) == FALSE)
+		const ULONG len = WINPR_ASSERTING_INT_CAST(ULONG, Stream_Capacity(s));
+		if (WTSVirtualChannelRead(audin->audin_channel, 0, Stream_BufferAs(s, char), len,
+		                          &BytesReturned) == FALSE)
 		{
 			WLog_Print(audin->log, WLOG_ERROR, "WTSVirtualChannelRead failed!");
 			error = ERROR_INTERNAL_ERROR;
 			break;
 		}
 
-		if (!Stream_SetLength(s, BytesReturned))
+		if (!Stream_SafeSeek(s, BytesReturned))
 		{
 			error = ERROR_INTERNAL_ERROR;
 			break;

channels/audin/client/audin_main.c
@@ -113,9 +113,10 @@ static UINT audin_channel_write_and_free(AUDIN_CHANNEL_CALLBACK* callback, wStre
 		return ERROR_INTERNAL_ERROR;
 
 	Stream_SealLength(out);
-	WINPR_ASSERT(Stream_Length(out) <= UINT32_MAX);
-	const UINT error = callback->channel->Write(callback->channel, (ULONG)Stream_Length(out),
-	                                            Stream_Buffer(out), nullptr);
+
+	const ULONG len = WINPR_ASSERTING_INT_CAST(ULONG, Stream_Length(out));
+	const UINT error =
+	    callback->channel->Write(callback->channel, len, Stream_Buffer(out), nullptr);
 
 	if (freeStream)
 		Stream_Free(out, TRUE);

commit cd6996c55712f824a620176cec46b8d82adc451e

[channels,audin] fix regression for microphone
In #12568 it was reported 20ed264d5d introduced a regression. Revert the
offending commit and add a stream length check as was intended with the
change.

channels/audin/server/audin.c
@@ -404,7 +404,17 @@ static DWORD WINAPI audin_server_thread_func(LPVOID arg)
 			break;
 		}
 
-		if (!Stream_SafeSeek(s, BytesReturned))
+		if (BytesReturned > len)
+		{
+			WLog_Print(audin->log, WLOG_ERROR,
+			           "WTSVirtualChannelRead returned an invalid length, got %" PRIu32
+			           ", but limit is %" PRIu32,
+			           BytesReturned, len);
+			error = ERROR_INTERNAL_ERROR;
+			break;
+		}
+
+		if (!Stream_SetLength(s, BytesReturned))
 		{
 			error = ERROR_INTERNAL_ERROR;
 			break;