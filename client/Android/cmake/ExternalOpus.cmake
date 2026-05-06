321a65499ecb1b67982a9b2a036e7b8d8984b26b
akallabeth <akallabeth@posteo.net>
2026-05-06 19:59:25 +0200

    [client,android] download opus from github

--- client/Android/cmake/ExternalOpus.cmake
+++ client/Android/cmake/ExternalOpus.cmake
@@ -3,9 +3,8 @@ include(ExternalProject)
 set(OPUS_VERSION "1.6.1")
 
 ExternalProject_Add(
-  opus SOURCE_DIR ${CMAKE_SOURCE_DIR}/external/opus
-  URL https://downloads.xiph.org/releases/opus/opus-${OPUS_VERSION}.tar.gz
-  URL_HASH SHA256=6ffcb593207be92584df15b32466ed64bbec99109f007c82205f0194572411a1
+  opus SOURCE_DIR ${CMAKE_SOURCE_DIR}/external/opus GIT_REPOSITORY https://github.com/xiph/opus GIT_TAG v1.6.1
+  GIT_SHALLOW TRUE
   CMAKE_ARGS ${ANDROID_CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX:PATH=${DEPS_INSTALL_DIR} -DCMAKE_INSTALL_LIBDIR:STRING=lib
              -DOPUS_BUILD_PROGRAMS:BOOL=OFF -DOPUS_BUILD_TESTING:BOOL=OFF
 )
